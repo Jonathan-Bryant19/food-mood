2648b71bcce6801806d5b93cce5b4c64c063ac27
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-14 10:01:19 -0800

    Added several components

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='topnav'>
+        <a href='#home' className='active'>Home</a>
+        <a href='#cart'>Orders</a>
+        <a href='#history'>History</a>
+    </div>
+  )
+}
+
+export default Navbar
