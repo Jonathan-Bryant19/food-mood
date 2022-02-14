237a68c6be422575df1bf97e5309cc10a7082d01
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-14 09:15:17 -0800

    Began adding framework for home page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Welcome, User!</h1>
+      </div>
+      <div className='topnav'>
+        <a href='#home' className='active'>Home</a>
+        <a href='#cart'>Orders</a>
+        <a href='#history'>History</a>
+      </div>
+      <div>
+        <input type="text"></input>
+      </div>
+    </>
   );
 }
 
