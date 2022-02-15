04ac6b4b6bc862ea45f9be9d63320ad1493dd57d
sam9750 <sam.wadhwa95@gmail.com>
2022-02-15 11:43:55 -0500

    added Table

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <div>
+      <table style={{width:'100%' }}>
+        <tr> 
+          <th>Product </th>
+          <th>Price </th>
+          <th>Quantity </th>
+          <th>Total </th>
+        </tr>
+        <tr>
+          <td> product1  </td>
+          <td> fsf</td>
+          <td>fs  </td>
+          <td>asfd  </td>
+
+        </tr>
+
+
+      </table> 
+    </div>
   )
 }
+
+
 
 export default Cart
